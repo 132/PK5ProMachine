22c6219fbc85756766779abdeff20efc690749ca
Hong3 <bialan132@gmail.com>
2018-06-26 16:23:44 +0200

    modified the server version

--- ConnectionClient/server2Client.py
+++ ConnectionClient/server2Client.py
@@ -2,9 +2,13 @@ from socket import socket, gethostbyname, AF_INET, SOCK_DGRAM, SOCK_STREAM
 #from subprocess import call ## does not work in this case
 import os
 import commands
+import select
+#import re				# regular expression
 
 PORT_NUMBER = 51236 
 SIZE = 1024
+backlog = 10			# the number of connection can have
+
 
 hostName = gethostbyname( '192.168.1.3' )
 
@@ -13,8 +17,9 @@ mySocket.bind( (hostName, PORT_NUMBER) )
 
 print ("Test server listening on port {0}\n".format(PORT_NUMBER))
 
-
-mySocket.listen(1)
+mySocket.listen(backlog)
+input_ = [mySocket,]
+"""
 while True:
 	print  'waiting for a connection'
 	connection, client_address = mySocket.accept()
@@ -33,6 +38,8 @@ while True:
 				connection.close()
 				break
 #				sys.exit()
+			elif not data:
+				break
 			else:
 				status, output = commands.getstatusoutput(data)
 				print status
@@ -45,7 +52,35 @@ while True:
 	finally:
 		print 'complete 1 connection'
 #connection.close()		
-
+"""
 #(data,addr) = mySocket.recvfrom(SIZE)
 #print data
 #sys.ext()
+
+while True:
+	inputReady, outputReady, exceptReady = select.select(input_, [], [])
+	
+	# inputReady a list of connections 
+	for s in inputReady:
+		if s == mySocket:
+			client, address = mySocket.accept()
+			input_.append(client)
+			print 'new client ' + str(address)
+		else:
+			data = s.recv(SIZE)
+			if data == 'kill_server':
+				s.close()
+				break
+			elif not data:
+				break
+			else:
+				print data
+				# filter before applying to rippled e.g:  Couldn't create directory monitor on smb://x-gnome-default-workgroup/.
+				#data = data.replace("'", "\'")
+				#data = data.replace('"', '\"')
+				status, output = commands.getstatusoutput(data)
+				print status
+				print '=============================='
+				print output
+				s.sendall(output)
+server.close()
